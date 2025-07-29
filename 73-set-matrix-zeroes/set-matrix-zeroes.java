ab77bd34e67913dd4ea47abd703169c7413e15cf
Ishita Srivastava <ishitasrivastava2183@gmail.com>
2025-07-29 21:40:17 +0530

    Time: 1 ms (72.27%) | Memory: 45.6 MB (83.79%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,23 +1,32 @@
 class Solution {
     public void setZeroes(int[][] arr) {
         int m=arr.length,n=arr[0].length;
-        int[][] dup=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dup[i][j]=arr[i][j];
-            }
+        boolean[] row=new boolean[m];
+        boolean[] col=new boolean[n];
+//marking particular row and col
+for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        if(arr[i][j]==0){
+            row[i]=true;
+            col[j]=true;
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(dup[i][j]==0){
-                    for(int b=0;b<n;b++){
-                        arr[i][b]=0;
-                    }
-                    for(int a=0;a<m;a++){
-                        arr[a][j]=0;
-                    }
-                }
-            }
+    }
+}
+//set true rowa to 0
+for(int i=0;i<m;i++){
+    if(row[i]==true){
+        for(int j=0;j<n;j++){
+            arr[i][j]=0;
         }
+    }
+}
+//set true col to 0
+for(int j=0;j<n;j++){
+    if(col[j]==true){
+        for(int i=0;i<m;i++){
+            arr[i][j]=0;
+        }
+    }
+}
     }
 }
