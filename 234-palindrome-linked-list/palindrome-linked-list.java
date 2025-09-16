71c9531c3735ea65b6b8a868d8c05209680db5cc
Ishita Srivastava <ishitasrivastava2183@gmail.com>
2025-09-16 21:36:39 +0530

    Time: 4 ms (83.51%) | Memory: 68.8 MB (27.45%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -12,25 +12,19 @@ class Solution {
         return prev;
     }
     public boolean isPalindrome(ListNode head) {
-        if(head.next==null) return true;
-        //create a deep copy
-        ListNode newHead=new ListNode(head.val);
-        ListNode t1=head.next;
-        ListNode t2=newHead;
-        while(t1!=null){
-            ListNode temp=new ListNode(t1.val);
-            t2.next=temp;
-            t2=t2.next;
-            t1=t1.next;
-        }
-     newHead=reverseList(newHead);  
-     t1=head;
-     t2=newHead;
-     while(t1!=null){
-        if(t1.val!=t2.val) return false;
-        t2=t2.next;
-        t1=t1.next;
-     } 
-     return true;
+       ListNode slow=head;
+       ListNode fast=head;
+       while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+       }
+       ListNode j=reverseList(slow);
+       ListNode i=head;
+       while(j!=null){
+        if(i.val!=j.val) return false;
+        i=i.next;
+        j=j.next;
+       }
+       return true;
     }
 }
