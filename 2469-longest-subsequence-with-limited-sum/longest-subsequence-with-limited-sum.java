745803f843a611b2cc2a05142acc21970beb4229
Ishita Srivastava <ishitasrivastava2183@gmail.com>
2025-09-27 19:54:58 +0530

    Time: 6 ms (54.44%) | Memory: 44.7 MB (87.74%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -6,15 +6,15 @@ class Solution {
             nums[i]+=nums[i-1];
         }
             int[] ans=new int[m];
-            for(int j=0;j<m;j++){
+            for(int i=0;i<m;i++){
                 int lo=0,hi=n-1;
                 while(lo<=hi){
                     int mid=lo+(hi-lo)/2;
-                    if(nums[mid]>queries[j]){
+                    if(nums[mid]>queries[i]){
                     hi=mid-1;
                     }
                     else{
-                    ans[j]=Math.max(ans[j],mid+1);
+                    ans[i]=Math.max(ans[i],mid+1);
                     lo=mid+1;
                 }
             }
